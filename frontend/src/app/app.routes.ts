54711732a5e427c0600e88c1ce5900ba0a4bc02c
Preeti Singh <preetisingh@Preetis-MacBook-Air.local>
2026-02-16 23:34:55 +0530

    nav bar created

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Login } from './components/login/login';
+import { SignUp } from './components/sign-up/sign-up';
+import { Home } from './pages/home/home';
+import { About } from './pages/about/about';
+import { Contact } from './pages/contact/contact';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Login },
+  { path: 'signup', component: SignUp },
+  { path: 'home', component: Home },
+  { path: 'about', component: About },
+  { path: 'contact', component: Contact }
+];
