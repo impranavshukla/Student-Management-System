aa139be9cbe34aac99856d2f5be56aadfb1b6730
Pranav Shukla <impranavshukla@gmail.com>
2026-02-17 09:34:08 +0530

    Login working

--- frontend/src/app/components/login/login.ts
+++ frontend/src/app/components/login/login.ts
@@ -1,21 +1,27 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { email } from '@angular/forms/signals';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink,ReactiveFormsModule],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
-loginData:FormGroup=new FormGroup({
-  email:new FormControl('',[Validators.required,Validators.email]),
-  password:new FormControl('',[Validators.required,Validators.minLength(6)]),
-});
+  constructor(private http: HttpClient,private router:Router) {}
 
-onLogin(){
-  console.log(this.loginData.value);
-}
+  loginData: FormGroup = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+  });
+
+  onLogin() {
+    this.http.get(`http://localhost:3000/teachers?email=${this.loginData.value.email}&password=${this.loginData.value.password}`).subscribe((res) => {
+        localStorage.setItem('loginUser', JSON.stringify(res));
+        this.router.navigateByUrl('/home');
+      });
+  }
 }
