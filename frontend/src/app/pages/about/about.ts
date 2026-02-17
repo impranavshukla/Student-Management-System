e24aa5012ed59e4c1bfd4c70bea4d22e004f5556
Preeti Singh <preetisingh@Preetis-MacBook-Air.local>
2026-02-17 16:32:45 +0530

    about and contact section addded

--- frontend/src/app/pages/about/about.ts
+++ frontend/src/app/pages/about/about.ts
@@ -1,8 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Footer } from '../../components/footer/footer';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [CommonModule,Footer],
   templateUrl: './about.html',
   styleUrl: './about.css',
 })
--- frontend/src/app/pages/contact/contact.ts
+++ frontend/src/app/pages/contact/contact.ts
@@ -1,8 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Footer } from '../../components/footer/footer';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [CommonModule,Footer],
   templateUrl: './contact.html',
   styleUrl: './contact.css',
 })
